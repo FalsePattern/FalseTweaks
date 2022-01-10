32fcd2ad1606cb6244c2b1736b7b29e6298f80cf
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-01-10 22:11:22 +0100

    coremod bad

--- src/main/java/com/falsepattern/triangulator/ModInfo.java
+++ src/main/java/com/falsepattern/triangulator/ModInfo.java
@@ -5,9 +5,4 @@ public class ModInfo {
     public static final String MODNAME = "GRADLETOKEN_MODNAME";
     public static final String VERSION = "GRADLETOKEN_VERSION";
     public static final String GROUPNAME = "GRADLETOKEN_GROUPNAME";
-
-    public static final String URL = "https://github.com/FalsePattern/Triangulator";
-    public static final String CREDITS = "";
-    public static final String[] AUTHORS = new String[]{"FalsePattern"};
-    public static final String DESCRIPTION = "A small library for modernizing the way minecraft renders quads.";
 }
--- src/main/java/com/falsepattern/triangulator/Triangulator.java
+++ src/main/java/com/falsepattern/triangulator/Triangulator.java
@@ -1,26 +1,16 @@
 package com.falsepattern.triangulator;
 
-import cpw.mods.fml.common.DummyModContainer;
-import cpw.mods.fml.common.ModMetadata;
-import lombok.val;
+import cpw.mods.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import scala.actors.threadpool.Arrays;
 
-public class Triangulator extends DummyModContainer {
+@Mod(modid = ModInfo.MODID,
+     version = ModInfo.VERSION,
+     name = ModInfo.MODNAME)
+public class Triangulator {
     public static Logger triLog = LogManager.getLogger(ModInfo.MODNAME);
 
     public Triangulator() {
-        super(new ModMetadata());
-        val meta = getMetadata();
-        meta.modId = ModInfo.MODID;
-        meta.name = ModInfo.MODNAME;
-        meta.version = ModInfo.VERSION;
-        meta.url = ModInfo.URL;
-        meta.credits = ModInfo.CREDITS;
-        meta.authorList = Arrays.asList(ModInfo.AUTHORS);
-        meta.description = ModInfo.DESCRIPTION;
-        meta.useDependencyInformation = true;
         triLog.info("Skidaddle skidoodle, your quad is now a noodle!");
     }
 }
