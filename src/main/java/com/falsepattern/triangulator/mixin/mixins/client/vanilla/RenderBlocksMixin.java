fdede8e925d309ee41e86043a6bd9747a0ba604c
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-06-01 20:24:04 +0200

    implement @embeddedt's ambient occlusion patch from #9

--- src/main/java/com/falsepattern/triangulator/mixin/mixins/client/vanilla/RenderBlocksMixin.java
+++ src/main/java/com/falsepattern/triangulator/mixin/mixins/client/vanilla/RenderBlocksMixin.java
@@ -4,10 +4,13 @@ import com.falsepattern.triangulator.mixin.helper.ITessellatorMixin;
 import lombok.val;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RenderBlocks.class)
@@ -69,6 +72,138 @@ public abstract class RenderBlocksMixin {
         } else if (altDiagonalDiff < mainDiagonalDiff) {
             ((ITessellatorMixin) Tessellator.instance).setAlternativeTriangulation();
         }
+    }
+
+    /**
+     * @author embeddedt
+     */
+    @ModifyArg(method = {"renderStandardBlockWithAmbientOcclusion", "renderStandardBlockWithAmbientOcclusionPartial"},
+               slice = @Slice(
+                       from = @At(value = "FIELD",
+                                  target = "Lnet/minecraft/client/renderer/RenderBlocks;aoLightValueScratchXYPN:F",
+                                  opcode = Opcodes.PUTFIELD,
+                                  ordinal = 0),
+                       to = @At(value = "FIELD",
+                                target = "Lnet/minecraft/client/renderer/RenderBlocks;aoLightValueScratchXYZNNN:F",
+                                opcode = Opcodes.PUTFIELD,
+                                ordinal = 0)
+               ),
+               at = @At(value = "INVOKE",
+                        target = "Lnet/minecraft/world/IBlockAccess;getBlock(III)Lnet/minecraft/block/Block;"),
+               index = 1,
+               allow = 8)
+    private int incrementYValue0(int y) {
+        return y + 1;
+    }
+
+    /**
+     * @author embeddedt
+     */
+    @ModifyArg(method = {"renderStandardBlockWithAmbientOcclusion", "renderStandardBlockWithAmbientOcclusionPartial"},
+               slice = @Slice(
+                       from = @At(value = "FIELD",
+                                  target = "Lnet/minecraft/client/renderer/RenderBlocks;aoLightValueScratchYZPP:F",
+                                  opcode = Opcodes.PUTFIELD,
+                                  ordinal = 0),
+                       to = @At(value = "FIELD",
+                                target = "Lnet/minecraft/client/renderer/RenderBlocks;aoLightValueScratchXYZNPN:F",
+                                opcode = Opcodes.PUTFIELD,
+                                ordinal = 0)
+               ),
+               at = @At(value = "INVOKE",
+                        target = "Lnet/minecraft/world/IBlockAccess;getBlock(III)Lnet/minecraft/block/Block;"),
+               index = 1,
+               allow = 8)
+    private int decrementYValue1(int y) {
+        return y - 1;
+    }
+
+    /**
+     * @author embeddedt
+     */
+    @ModifyArg(method = {"renderStandardBlockWithAmbientOcclusion", "renderStandardBlockWithAmbientOcclusionPartial"},
+               slice = @Slice(
+                       from = @At(value = "FIELD",
+                                  target = "Lnet/minecraft/client/renderer/RenderBlocks;aoBrightnessXZPN:I",
+                                  opcode = Opcodes.PUTFIELD,
+                                  ordinal = 0),
+                       to = @At(value = "FIELD",
+                                target = "Lnet/minecraft/client/renderer/RenderBlocks;aoLightValueScratchXYZNNN:F",
+                                opcode = Opcodes.PUTFIELD,
+                                ordinal = 2)
+               ),
+               at = @At(value = "INVOKE",
+                        target = "Lnet/minecraft/world/IBlockAccess;getBlock(III)Lnet/minecraft/block/Block;"),
+               index = 2,
+               allow = 8)
+    private int incrementZValue2(int z) {
+        return z + 1;
+    }
+
+    /**
+     * @author embeddedt
+     */
+    @ModifyArg(method = {"renderStandardBlockWithAmbientOcclusion", "renderStandardBlockWithAmbientOcclusionPartial"},
+               slice = @Slice(
+                       from = @At(value = "FIELD",
+                                  target = "Lnet/minecraft/client/renderer/RenderBlocks;aoLightValueScratchYZPP:F",
+                                  opcode = Opcodes.PUTFIELD,
+                                  ordinal = 1),
+                       to = @At(value = "FIELD",
+                                target = "Lnet/minecraft/client/renderer/RenderBlocks;aoLightValueScratchXYZNNP:F",
+                                opcode = Opcodes.PUTFIELD,
+                                ordinal = 2)
+               ),
+               at = @At(value = "INVOKE",
+                        target = "Lnet/minecraft/world/IBlockAccess;getBlock(III)Lnet/minecraft/block/Block;"),
+               index = 2,
+               allow = 8)
+    private int decrementZValue3(int z) {
+        return z - 1;
+    }
+
+    /**
+     * @author embeddedt
+     */
+    @ModifyArg(method = {"renderStandardBlockWithAmbientOcclusion", "renderStandardBlockWithAmbientOcclusionPartial"},
+               slice = @Slice(
+                       from = @At(value = "FIELD",
+                                  target = "Lnet/minecraft/client/renderer/RenderBlocks;aoLightValueScratchXYNP:F",
+                                  opcode = Opcodes.PUTFIELD,
+                                  ordinal = 1),
+                       to = @At(value = "FIELD",
+                                target = "Lnet/minecraft/client/renderer/RenderBlocks;aoLightValueScratchXYZNNN:F",
+                                opcode = Opcodes.PUTFIELD,
+                                ordinal = 4)
+               ),
+               at = @At(value = "INVOKE",
+                        target = "Lnet/minecraft/world/IBlockAccess;getBlock(III)Lnet/minecraft/block/Block;"),
+               index = 0,
+               allow = 8)
+    private int incrementXValue4(int x) {
+        return x + 1;
+    }
+
+    /**
+     * @author embeddedt
+     */
+    @ModifyArg(method = {"renderStandardBlockWithAmbientOcclusion", "renderStandardBlockWithAmbientOcclusionPartial"},
+               slice = @Slice(
+                       from = @At(value = "FIELD",
+                                  target = "Lnet/minecraft/client/renderer/RenderBlocks;aoLightValueScratchXYPP:F",
+                                  opcode = Opcodes.PUTFIELD,
+                                  ordinal = 1),
+                       to = @At(value = "FIELD",
+                                target = "Lnet/minecraft/client/renderer/RenderBlocks;aoLightValueScratchXYZPNN:F",
+                                opcode = Opcodes.PUTFIELD,
+                                ordinal = 4)
+               ),
+               at = @At(value = "INVOKE",
+                        target = "Lnet/minecraft/world/IBlockAccess;getBlock(III)Lnet/minecraft/block/Block;"),
+               index = 0,
+               allow = 8)
+    private int decrementXValue5(int x) {
+        return x - 1;
     }
 
     private static float avg(final float a, final float b) {
