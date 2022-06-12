ec668a60b0c993bfc7172808ee549a539194592e
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-06-12 22:45:54 +0200

    so apparently mixins can mutate finals. This feels dirty.

--- src/main/java/com/falsepattern/animfix/interfaces/IStitcherSlotMixin.java
+++ src/main/java/com/falsepattern/animfix/interfaces/IStitcherSlotMixin.java
@@ -5,4 +5,5 @@ import net.minecraft.client.renderer.texture.Stitcher;
 public interface IStitcherSlotMixin {
     void insertHolder(Stitcher.Holder holder);
     void insertSlot(Stitcher.Slot slot);
+    void reparent(int offsetX, int offsetY);
 }
--- src/main/java/com/falsepattern/animfix/mixin/mixins/client/minecraft/StitcherSlotMixin.java
+++ src/main/java/com/falsepattern/animfix/mixin/mixins/client/minecraft/StitcherSlotMixin.java
@@ -1,8 +1,11 @@
 package com.falsepattern.animfix.mixin.mixins.client.minecraft;
 
 import com.falsepattern.animfix.interfaces.IStitcherSlotMixin;
+import lombok.val;
 import net.minecraft.client.renderer.texture.Stitcher;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.ArrayList;
@@ -16,9 +19,26 @@ public abstract class StitcherSlotMixin implements IStitcherSlotMixin {
 
     @Shadow private List subSlots;
 
+    @Mutable
+    @Shadow @Final private int originX;
+
+    @Mutable
+    @Shadow @Final private int originY;
+
     @Override
     public void insertHolder(Stitcher.Holder holder) {
         this.holder = holder;
+    }
+
+    @Override
+    public void reparent(int offsetX, int offsetY) {
+        originX += offsetX;
+        originY += offsetY;
+        if (this.subSlots != null) {
+            for (val subSlot: subSlots) {
+                ((IStitcherSlotMixin)subSlot).reparent(offsetX, offsetY);
+            }
+        }
     }
 
     @Override
@@ -26,6 +46,7 @@ public abstract class StitcherSlotMixin implements IStitcherSlotMixin {
         if (this.subSlots == null) {
             this.subSlots = new ArrayList();
         }
+        ((IStitcherSlotMixin)slot).reparent(originX, originY);
         this.subSlots.add(slot);
     }
 }
