62d211411e8c9a7f648c5fd228ad1701fd50cf57
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-06-27 18:07:07 +0200

    neodymium compatibility will be implemented on their side

--- src/main/java/com/falsepattern/triangulator/TriCompat.java
+++ src/main/java/com/falsepattern/triangulator/TriCompat.java
@@ -4,8 +4,7 @@ import com.falsepattern.triangulator.config.TriConfig;
 import cpw.mods.fml.common.Loader;
 
 public class TriCompat {
-    private static final boolean NEODYMIUM = Loader.isModLoaded("neodymium");
     public static boolean enableTriangulation() {
-        return TriConfig.ENABLE_QUAD_TRIANGULATION && !NEODYMIUM;
+        return TriConfig.ENABLE_QUAD_TRIANGULATION;
     }
 }
