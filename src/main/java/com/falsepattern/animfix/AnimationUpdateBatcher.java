b709b0df0ab8d83a844771516d1b423a6d4ee8fb
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-08-26 19:22:19 +0200

    faster buffer logic

--- src/main/java/com/falsepattern/animfix/AnimationUpdateBatcher.java
+++ src/main/java/com/falsepattern/animfix/AnimationUpdateBatcher.java
@@ -24,32 +24,45 @@ package com.falsepattern.animfix;
 import lombok.SneakyThrows;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
+import sun.misc.Unsafe;
 
 import net.minecraft.client.renderer.GLAllocation;
 import net.minecraft.client.renderer.texture.TextureMap;
 
+import cpw.mods.fml.relauncher.ReflectionHelper;
+
+import java.nio.Buffer;
 import java.nio.IntBuffer;
 
 public class AnimationUpdateBatcher {
     public static TextureMap currentAtlas = null;
     public static AnimationUpdateBatcher batcher = null;
+    private static final Unsafe unsafe = ReflectionHelper.getPrivateValue(Unsafe.class, null, "theUnsafe");
+    private static final long arrayBaseOffset = unsafe.arrayBaseOffset(int[].class);
     private final int mipLevels;
     private final int xOffset;
     private final int yOffset;
     private final int width;
     private final int height;
-    private final IntBuffer[] data;
+    private final int[] offsets;
+    private final IntBuffer memory;
+    private final long basePointer;
 
+    @SneakyThrows
     public AnimationUpdateBatcher(int xOffset, int yOffset, int width, int height, int mipLevel) {
         this.mipLevels = mipLevel;
         this.xOffset = xOffset;
         this.yOffset = yOffset;
         this.width = width;
         this.height = height;
-        data = new IntBuffer[mipLevel + 1];
+        offsets = new int[mipLevel + 1];
+        int size = 0;
         for (int i = 0; i <= mipLevel; i++) {
-            data[i] = GLAllocation.createDirectIntBuffer((width >>> i) * (height >>> i));
+            offsets[i] = size;
+            size += (width >>> i) * (height >>> i);
         }
+        memory = GLAllocation.createDirectIntBuffer(size);
+        basePointer = ReflectionHelper.getPrivateValue(Buffer.class, memory, "address");
     }
 
     public boolean batchUpload(int[][] texture, int width, int height, int xOffset, int yOffset) {
@@ -60,12 +73,9 @@ public class AnimationUpdateBatcher {
         }
         int w = this.width;
         for (int mipMapLevel = 0; mipMapLevel < texture.length; mipMapLevel++) {
-            IntBuffer atlasTexture = data[mipMapLevel];
-
-            int base = yOffset * w + xOffset;
+            long base = basePointer + ((offsets[mipMapLevel] + (long) yOffset * w + xOffset) << 2);
             for (int i = 0; i < height; i++) {
-                atlasTexture.position(base + i * w);
-                atlasTexture.put(texture[mipMapLevel], i * width, width);
+                unsafe.copyMemory(texture[mipMapLevel], arrayBaseOffset + (((long) i * width) << 2), null, base + (((long) i * w)<< 2), (long) width << 2);
             }
             xOffset >>>= 1;
             yOffset >>>= 1;
@@ -79,10 +89,9 @@ public class AnimationUpdateBatcher {
     @SneakyThrows
     public void upload() {
         for (int i = 0; i <= mipLevels; i++) {
-            IntBuffer buf = data[i];
-            buf.position(0);
+            memory.position(offsets[i]);
             GL11.glTexSubImage2D(GL11.GL_TEXTURE_2D, i, xOffset >>> i, yOffset >>> i, width >>> i, height >>> i,
-                                 GL12.GL_BGRA, GL12.GL_UNSIGNED_INT_8_8_8_8_REV, buf);
+                                 GL12.GL_BGRA, GL12.GL_UNSIGNED_INT_8_8_8_8_REV, memory);
         }
     }
 }
