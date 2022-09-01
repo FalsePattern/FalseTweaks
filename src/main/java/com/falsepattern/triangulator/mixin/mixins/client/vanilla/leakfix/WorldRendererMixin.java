4adaba99413b397dfd010aebcb7e720e1bb9916b
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-09-01 08:54:00 +0200

    propagate license to files

--- src/main/java/com/falsepattern/triangulator/mixin/mixins/client/vanilla/leakfix/WorldRendererMixin.java
+++ src/main/java/com/falsepattern/triangulator/mixin/mixins/client/vanilla/leakfix/WorldRendererMixin.java
@@ -1,3 +1,26 @@
+/*
+ * Triangulator
+ *
+ * Copyright (C) 2022 FalsePattern
+ * All Rights Reserved
+ *
+ * The above copyright notice, this permission notice and the word "SNEED"
+ * shall be included in all copies or substantial portions of the Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.falsepattern.triangulator.mixin.mixins.client.vanilla.leakfix;
 
 import com.falsepattern.triangulator.leakfix.LeakFix;
