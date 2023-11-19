bcdf69b3ea99cd125d131854c90b3661ac801796
FalsePattern <me@falsepattern.com>
2023-11-19 16:25:47 +0100

    Update license text on files

--- src/main/java/com/falsepattern/falsetweaks/modules/animfix/DefaultAnimationUpdateBatcher.java
+++ src/main/java/com/falsepattern/falsetweaks/modules/animfix/DefaultAnimationUpdateBatcher.java
@@ -1,24 +1,18 @@
 /*
- * FalseTweaks
+ * This file is part of FalseTweaks.
  *
- * Copyright (C) 2022 FalsePattern
- * All Rights Reserved
- *
- * The above copyright notice, this permission notice and the word "SNEED"
- * shall be included in all copies or substantial portions of the Software.
- *
- * This program is free software: you can redistribute it and/or modify
+ * FalseTweaks is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * FalseTweaks is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with FalseTweaks. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.falsepattern.falsetweaks.modules.animfix;
