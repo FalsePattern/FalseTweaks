18d4bee8bc798223e78ded2b4e2f30b23af2291d
FalsePattern <me@falsepattern.com>
2025-05-06 01:22:47 +0200

    update buildscript and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("fpgradle-minecraft") version ("0.10.1")
+    id("com.falsepattern.fpgradle-mc") version ("0.15.1")
 }
 
 group = "com.falsepattern"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,33 +1,3 @@
-pluginManagement {
-    repositories {
-        maven {
-            url = uri("https://mvn.falsepattern.com/fpgradle/")
-            name = "fpgradle"
-            content {
-                includeModule("com.gtnewhorizons", "retrofuturagradle")
-                includeModule("com.falsepattern", "fpgradle-plugin")
-                includeModule("fpgradle-minecraft", "fpgradle-minecraft.gradle.plugin")
-            }
-        }
-        maven {
-            url = uri("https://mvn.falsepattern.com/releases/")
-            name = "mavenpattern"
-            content {
-                includeGroup("com.falsepattern")
-            }
-        }
-        maven {
-            url = uri("https://mvn.falsepattern.com/jitpack/")
-            name = "jitpack"
-            content {
-                includeModule("io.github.LegacyModdingMC.MappingGenerator", "MappingGenerator")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.9.0")
 }
