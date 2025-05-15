e20d5147b8fd5f083e85f1a7da2b7ddea6efacae
FalsePattern <me@falsepattern.com>
2025-05-15 14:56:54 +0200

    update license

--- src/main/java/com/falsepattern/falsetweaks/asm/modules/threadedupdates/settings/Threading_GameSettings.java
+++ src/main/java/com/falsepattern/falsetweaks/asm/modules/threadedupdates/settings/Threading_GameSettings.java
@@ -2,7 +2,7 @@
 /*
  * This file is part of FalseTweaks.
  *
- * Copyright (C) 2022-2024 FalsePattern
+ * Copyright (C) 2022-2025 FalsePattern
  * All Rights Reserved
  *
  * The above copyright notice and this permission notice shall be included
@@ -10,8 +10,7 @@
  *
  * FalseTweaks is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, only version 3 of the License.
  *
  * FalseTweaks is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
