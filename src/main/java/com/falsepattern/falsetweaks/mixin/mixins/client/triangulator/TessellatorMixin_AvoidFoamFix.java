bd4d1fcc5ab1a8ba1b21fa6bb7b2de29f38df860
FalsePattern <me@falsepattern.com>
2025-10-14 19:18:43 +0200

    fix: Triangulator broken without BSP sorting enabled

---
+++ src/main/java/com/falsepattern/falsetweaks/mixin/mixins/client/triangulator/TessellatorMixin_AvoidFoamFix.java
@@ -0,0 +1,40 @@
+/*
+ * Swansong
+ *
+ * Copyright 2025 Ven, FalsePattern
+ *
+ * This software is licensed under the Open Software License version
+ * 3.0. The full text of this license can be found in https://opensource.org/licenses/OSL-3.0
+ * or in the LICENSES directory which is distributed along with the software.
+ */
+
+package com.falsepattern.falsetweaks.mixin.mixins.client.triangulator;
+
+import com.falsepattern.falsetweaks.modules.triangulator.TriComparator;
+import com.falsepattern.falsetweaks.modules.triangulator.interfaces.ITriangulatorTessellator;
+import com.falsepattern.falsetweaks.modules.vertexapi.VertexInfo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.util.QuadComparator;
+
+@Mixin(value = Tessellator.class,
+       //Hodgepodge compat
+       priority = 900)
+public abstract class TessellatorMixin_AvoidFoamFix implements ITriangulatorTessellator {
+    @Redirect(method = "getVertexState",
+              at = @At(value = "NEW",
+                       target = "([IFFF)Lnet/minecraft/client/util/QuadComparator;"),
+              require = 1)
+    private QuadComparator hackQuadComparator(int[] vertexData, float x, float y, float z) {
+        if (this.drawingTris()) {
+            return new TriComparator(vertexData, x, y, z);
+        } else {
+            return new QuadComparator(vertexData, x, y, z);
+        }
+    }
+}
